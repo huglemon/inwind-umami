b042ded2a020231df63d652fc8cba7fc8bfbb0d1
huglemon <hi@huglemon.com>
2026-02-27 12:01:15 +0800

    Update footer and header components to display the name "乘风出海" and modify the logo SVG for improved design and responsiveness.

--- src/app/share/[...shareId]/Header.tsx
+++ src/app/share/[...shareId]/Header.tsx
@@ -6,12 +6,12 @@ import { Logo } from '@/components/svg';
 export function Header() {
   return (
     <Row as="header" justifyContent="space-between" alignItems="center" paddingY="3">
-      <a href="https://umami.is" target="_blank" rel="noopener">
+      <a href="https://inwind.cn?utm_source=umami_share" target="_blank" rel="noopener">
         <Row alignItems="center" gap>
           <Icon>
             <Logo />
           </Icon>
-          <Text weight="bold">umami</Text>
+          <Text weight="bold">乘风出海</Text>
         </Row>
       </a>
       <Row alignItems="center" gap>
