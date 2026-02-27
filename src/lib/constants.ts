415222666c4f08666608aec0c1a401d0adba58ab
huglemon <hi@huglemon.com>
2026-02-27 12:32:59 +0800

    Update application components to consistently display "乘风出海" and modify the homepage URL for localization.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,7 +8,7 @@ export const DASHBOARD_CONFIG = 'umami.dashboard';
 export const LAST_TEAM_CONFIG = 'umami.last-team';
 export const VERSION_CHECK = 'umami.version-check';
 export const SHARE_TOKEN_HEADER = 'x-umami-share-token';
-export const HOMEPAGE_URL = 'https://umami.is';
+export const HOMEPAGE_URL = 'https://inwind.cn?from=umami_share';
 export const DOCS_URL = 'https://umami.is/docs';
 export const REPO_URL = 'https://github.com/umami-software/umami';
 export const UPDATES_URL = 'https://api.umami.is/v1/updates';
